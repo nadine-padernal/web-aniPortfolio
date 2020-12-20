df774ff270a5c86a437376bb47e153b36da84497
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-12-20 16:18:14 +0800

    Update aboutMe.php

--- aboutMe.php
+++ aboutMe.php
@@ -13,8 +13,19 @@ $module = (isset($_GET['mod']) && $_GET['mod'] != '') ? $_GET['mod'] : '';
 
 <body>
     <div id="content">
-      <h1> Noelle Nadine G. Padernal</h1>
-      <h3> BSIT 3-A</h3>
+      <div class="abtSVG">
+    <svg viewBox="0 0 300 500">
+	    <text x="0%" y="20%" dy=".35em" text-anchor="middle">
+		  Nadine Padernal
+	    </text>
+    </svg>	
+    <svg viewBox="0 0 300 500">
+	    <text class="sub-text" x="0%" y="30%" dy=".35em" text-anchor="middle">
+		  "Nadz Pads"
+	    </text>
+    </svg>
+</div>	
+    <p> As of December 2020, I'm a 20 year old, third year Information Technology student. I have prior knowledge about Computer Hardware Servicing and a beginner in AutoCAD. My skills on designing websites had drastically improve compared to my first couple of years as a college student. There are still a lot left for me to learn and room for improvement. <br><br>"Never stop learning, because life never stops teaching." </p>
     </div>
 </body>
 
